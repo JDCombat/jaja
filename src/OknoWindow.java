41800349c70aa414ae99d978b8ba895fdd64126d
JDCombat <bartekkozub2007@gmail.com>
2024-04-29 21:59:58 +0200

    jak ja nienawidzę bozgiego

---
+++ src/OknoWindow.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class OknoWindow extends JFrame{
+    private JPanel panel;
+    private JTextField amount;
+    private JButton deposit;
+    private JButton all;
+    private JLabel notifications;
+    private JLabel inValue;
+    private JLabel out;
+    private JLabel outValue;
+    private JLabel balance;
+    private JLabel balanceValue;
+    private JLabel in;
+    private JButton choose;
+
+    public OknoWindow(){
+        setContentPane(panel);
+        setTitle("Okno");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        OknoWindow okno = new OknoWindow();
+    }
+}
