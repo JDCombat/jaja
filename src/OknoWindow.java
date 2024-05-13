da517207fe239c545b74e7ba5a8c63fd7dc8aad0
JDCombat <bartekkozub2007@gmail.com>
2024-05-13 22:47:20 +0200

    pozdrawiam mamę michała

--- src/OknoWindow.java
+++ src/OknoWindow.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OknoWindow extends JFrame{
     private JPanel panel;
@@ -13,13 +15,24 @@ public class OknoWindow extends JFrame{
     private JLabel balanceValue;
     private JLabel in;
     private JButton choose;
+    private JLabel notif;
+
+    private int money;
 
     public OknoWindow(){
         setContentPane(panel);
         setTitle("Okno");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,200);
+        setSize(500,300);
         setVisible(true);
+
+        deposit.addActionListener(e -> {
+            money += Integer.parseInt(amount.getText());
+            inValue.setText(money + "zł");
+            balanceValue.setText("" + money);
+            notif.setText(notif.getText() + "Wpłacono " + money + "zł");
+            amount.setText("");
+        });
     }
 
     public static void main(String[] args) {
